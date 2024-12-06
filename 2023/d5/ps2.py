91c1372f8a50207fd71aecd2c4bb7250cf4b670d
Syed Adeed <syedadeed@protonmail.com>
2024-12-06 12:56:36 +0530

    update

--- 2023/d5/ps2.py
+++ 2023/d5/ps2.py
@@ -22,17 +22,25 @@ def process_input() -> tuple:
 def find_mapping(map_values, source) -> list:
     maping = {i: i for i in source}
 
-    for rq_source_range in source:
-        for i in range(len(map_values)):
-            av_destination_range = range(map_values[i][0], map_values[i][0] + map_values[i][2])
-            av_source_range = range(map_values[i][1], map_values[i][1] + map_values[i][2])
+    for i in range(len(map_values)):
+        av_destination_range = range(map_values[i][0], map_values[i][0] + map_values[i][2])
+        av_source_range = range(map_values[i][1], map_values[i][1] + map_values[i][2])
 
+        for rq_source_range in list(maping.keys()):
             common_start = max(av_source_range.start, rq_source_range.start)
             common_end = min(av_source_range.stop, rq_source_range.stop)
+
             if common_start < common_end:
+                maping.pop(rq_source_range)
                 x1 = common_start - av_source_range.start
                 x2 = common_end - av_source_range.stop
-                maping.update({rq_source_range: range(av_destination_range.start + x1, av_destination_range.stop + x2)})
+                r1 = range(rq_source_range.start, common_start)
+                r2 = range(common_end, rq_source_range.stop)
+                if len(r1) > 0:
+                    maping.update({r1: r1})
+                maping.update({range(common_start, common_end): range(av_destination_range.start + x1, av_destination_range.stop + x2)})
+                if len(r2) > 0:
+                    maping.update({r2: r2})
 
     return list(maping.values())
 
@@ -43,3 +51,5 @@ for i in range(0, len(data[0]), 2):
 
 for i in data[1:]:
     location = find_mapping(i, location)
+
+print(min([i.start for i in location]))
